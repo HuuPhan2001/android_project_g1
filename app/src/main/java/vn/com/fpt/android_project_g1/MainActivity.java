d00831e6ca0cabab09c731e1380f48dd50da00b0
BasHuw <huupbhe150420@fpt.edu.vn>
2022-10-24 11:39:15 +0700

    project_android

---
+++ app/src/main/java/vn/com/fpt/android_project_g1/MainActivity.java
@@ -0,0 +1,13 @@
+package vn.com.fpt.android_project_g1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
