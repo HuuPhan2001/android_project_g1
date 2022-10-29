cbdd54239f959e99a0ff6785d3a171b3168b43be
egamorft <huytung.htbn@gmail.com>
2022-10-29 08:15:41 +0700

    Quiz list, adjust timer clock to transparent png

--- app/src/main/java/vn/com/fpt/android_project_g1/DashboardActivity.java
+++ app/src/main/java/vn/com/fpt/android_project_g1/DashboardActivity.java
@@ -1,6 +1,9 @@
 package vn.com.fpt.android_project_g1;
 
+import static vn.com.fpt.android_project_g1.MainActivity.list;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.widget.ContentLoadingProgressBar;
 
 import android.app.Dialog;
@@ -9,19 +12,34 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity {
 
     CountDownTimer countDownTimer;
     int timerValue=20;
     ContentLoadingProgressBar progressBar;
+    List<QuestionModel> allQuestionList;
+    QuestionModel questionModel;
+    int index = 0;
+    TextView card_question, optiona, optionb, optionc, optiond;
+    CardView cardOA, cardOB, cardOC, cardOD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        progressBar=findViewById(R.id.quiz_timer);
+        Hooks();
+
+        allQuestionList = list;
+        Collections.shuffle(allQuestionList);
+        questionModel = list.get(index);
+
+        setAllData();
 
         countDownTimer=new CountDownTimer( 20000,1000) {
             @Override
@@ -48,5 +66,28 @@ public class DashboardActivity extends AppCompatActivity {
 
             }
         }.start();
+    }
+
+    private void Hooks(){
+        progressBar = findViewById(R.id.quiz_timer);
+
+        card_question = findViewById(R.id.card_question);
+        optiona = findViewById(R.id.card_optionA);
+        optionb = findViewById(R.id.card_optionB);
+        optionc = findViewById(R.id.card_optionC);
+        optiond = findViewById(R.id.card_optionD);
+
+        cardOA = findViewById(R.id.cardA);
+        cardOB = findViewById(R.id.cardB);
+        cardOC = findViewById(R.id.cardC);
+        cardOD = findViewById(R.id.cardD);
+    }
+
+    private void setAllData() {
+        card_question.setText(questionModel.getQuestion());
+        optiona.setText(questionModel.getoA());
+        optionb.setText(questionModel.getoB());
+        optionc.setText(questionModel.getoC());
+        optiond.setText(questionModel.getoD());
     }
 }
--- app/src/main/java/vn/com/fpt/android_project_g1/MainActivity.java
+++ app/src/main/java/vn/com/fpt/android_project_g1/MainActivity.java
@@ -5,12 +5,34 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static ArrayList<QuestionModel> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = new ArrayList<>();
+        list.add(new QuestionModel("CauHoi1", "1", "2", "3", "4", "2"));
+        list.add(new QuestionModel("CauHoi2", "1", "2", "3", "4", "3"));
+        list.add(new QuestionModel("CauHoi3", "1", "2", "3", "4", "1"));
+        list.add(new QuestionModel("CauHoi4", "1", "2", "3", "4", "3"));
+        list.add(new QuestionModel("CauHoi5", "1", "2", "3", "4", "2"));
+        list.add(new QuestionModel("CauHoi6", "1", "2", "3", "4", "2"));
+        list.add(new QuestionModel("CauHoi7", "1", "2", "3", "4", "4"));
+        list.add(new QuestionModel("CauHoi8", "1", "2", "3", "4", "1"));
+        list.add(new QuestionModel("CauHoi9", "1", "2", "3", "4", "2"));
+        list.add(new QuestionModel("CauHoi10", "1", "2", "3", "4", "3"));
+        list.add(new QuestionModel("CauHoi11", "1", "2", "3", "4", "3"));
+        list.add(new QuestionModel("CauHoi12", "1", "2", "3", "4", "4"));
+        list.add(new QuestionModel("CauHoi13", "1", "2", "3", "4", "4"));
+        list.add(new QuestionModel("CauHoi14", "1", "2", "3", "4", "1"));
+        list.add(new QuestionModel("CauHoi15", "1", "2", "3", "4", "1"));
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
